a66f7c2427a96b962ee89f5e9bacfb60b71fe28a
Sam Lu <pbcmarsh@gmail.com>
2023-07-13 19:44:20 -0500

    Introductory Problems
    Introductory Problems

---
+++ Introductory Problems/WeirdAlgorithm.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+// Time:  Unclear
+// Space: O(1)
+public class WeirdAlgorithm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.print(n);
+        while (n != 1) {
+            if (n % 2 == 1) {
+                n *= 3;
+                n++;
+            } else {
+                n >>= 1;
+            }
+            System.out.print(" " + n);
+        }
+    }
+}
