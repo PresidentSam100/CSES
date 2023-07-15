fbeed17c3fb464885809ca326c1ab8f00c6720b3
Sam Lu <pbcmarsh@gmail.com>
2023-07-15 14:46:12 -0500

    Create BitStrings.cpp

---
+++ Introductory Problems/BitStrings.cpp
@@ -0,0 +1,21 @@
+// https://cses.fi/problemset/task/1617
+// Bit Strings
+ 
+#include <iostream>
+#include <math.h>
+#define lli long long int
+using namespace std;
+ 
+lli n, power;
+lli MOD = 1000000007;
+ 
+int main() {
+	cin >> n;
+	power = 1;
+	for (lli i = 0; i < n; i++) {
+	    power *= 2;
+	    power %= MOD;
+	}
+	cout << power;
+	return 0;
+}
