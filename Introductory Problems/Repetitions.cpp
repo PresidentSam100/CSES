c5e80719ef76844b61785e5f93e2a11dc89f3f93
Sam Lu <pbcmarsh@gmail.com>
2023-07-15 14:39:32 -0500

    Create Repetitions.cpp

---
+++ Introductory Problems/Repetitions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+string str, prev;
+ 
+int main() {
+	cin >> str;
+	int ans = 1, rep = 1;
+	for (unsigned int i = 1; i < str.length(); i++) {
+		// Adjacent strings match 
+		if (str[i] == str[i - 1]) {
+			rep++;
+		}
+		// Adjacent strings don't match
+		else {
+			rep = 1;
+		}
+		// Update answer if necessary
+		ans = max(rep, ans);
+	}
+	cout << ans << endl;
+	return 0;
+}
