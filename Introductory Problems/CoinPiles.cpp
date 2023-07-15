0732fa5eea77820fa4a102fb2547909978d02b13
Sam Lu <pbcmarsh@gmail.com>
2023-07-15 14:47:14 -0500

    Create CoinPiles.cpp

---
+++ Introductory Problems/CoinPiles.cpp
@@ -0,0 +1,23 @@
+// https://cses.fi/problemset/task/1754
+// Coin Piles
+ 
+#include <iostream>
+#define lli long long int
+using namespace std;
+ 
+lli t, a, b;
+ 
+int main() {
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+		// Observation 1: Total number of coins (a + b) must be divisible by 3.
+		// Observation 2: Bigger pile can't have more than twice as much as the small pile
+		if ((a + b) % 3 == 0 && max(a, b) <= 2 * min(a, b)) {
+			cout << "YES" << endl;
+		} else {
+			cout << "NO" << endl;
+		}
+	}
+	return 0;
+}
