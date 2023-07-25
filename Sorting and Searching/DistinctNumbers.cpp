1e384fcab54794c7adccbb8fcc234101c75b5b3f
Sam Lu <pbcmarsh@gmail.com>
2023-07-25 11:55:50 -0500

    Add files via upload

---
+++ Sorting and Searching/DistinctNumbers.cpp
@@ -0,0 +1,30 @@
+// https://cses.fi/problemset/task/1621
+// Distinct Numbers
+ 
+#include <iostream>
+#include <algorithm>
+#define ll long long
+using namespace std;
+ 
+ll n, x, ans;
+ll arr[200000];
+ 
+int main() {
+	cin >> n;
+	// Get all values and store in array
+	for (ll i = 0; i < n; i++) {
+	    cin >> x;
+	    arr[i] = x;
+	}
+	// Sort array in ascending order
+	sort(arr, arr + n);
+	ans = 1;
+	for (ll i = 1; i < n; i++) {
+	    // If two adjacent values are distinct, add to ans
+	    if (arr[i] != arr[i - 1]) {
+	        ans++;
+	    }
+	}
+	cout << ans;
+	return 0;
+}
